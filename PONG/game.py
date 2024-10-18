d72b0f4217f9f5217bf20635637aad71cda8a6a1
LarChim01 <larchim@yandex.ru>
2024-10-18 14:16:33 +0300

    Update game.py

--- PONG/game.py
+++ PONG/game.py
@@ -1,22 +1,46 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+#from arcade import Sprite
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Pong Game"
 
 class Bar(arcade.Sprite):
     def __init__(self):
-        super().__init__('bar.png',0.05)
+        super().__init__('bar.png',0.4)
+
+
+    def update(self):
+        self.center_x += self.change_x
+
+        if self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left <= 0:
+            self.left= 0
 
 class Ball(arcade.Sprite):
     def __init__(self):
-        super().__init__('ball.png',1.0)
+        super().__init__('boll.png',0.06)
+        self.change_x = 3
+        self.change_y = 3
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if self.right >= SCREEN_WIDTH  or   self.left <= 0:
+            self.change_x = -self.change_x
+        if self.top <= 5  or   self.bottom >= SCREEN_HEIGHT :
+            self.change_y = -self.change_y
+
+
 
 class Game(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.bar = Bar()
         self.ball = Ball()
+        self.setup()
 
     def on_draw(self):
         self.clear((255, 255, 255))
@@ -26,10 +50,24 @@ class Game(arcade.Window):
 
     def setup(self):
         self.bar.center_x = SCREEN_WIDTH / 2
-        self.bar.center_y = SCREEN_HEIGHT / 5
+        self.bar.center_y = SCREEN_HEIGHT / 10
         self.ball.center_x = SCREEN_WIDTH / 2
-        self.ball.center_y = SCREEN_HEIGHT / 5
+        self.ball.center_y = SCREEN_HEIGHT / 3
 
+    def update(self, delta):
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.change_y = -self.ball.change_y 
+        self.ball.update()
+        self.bar.update()
+
+    def on_key_press(self,key ,modifiers):
+        if key == arcade.key.RIGHT:
+            self.bar.change_x = 5
+        if key == arcade.key.LEFT:
+            self.bar.change_x = -5
+    def on_key_release(self, key ,modifiers):
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.bar.change_x = 0
 
 
 
